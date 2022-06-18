e907e027c30afca76ee176dadfadfb56c2ac11f7
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2022-06-18 15:44:31 +0700

    .

--- Assignment/src/test/java/com/example/assignment/entity/DistrictTest.java
+++ Assignment/src/test/java/com/example/assignment/entity/DistrictTest.java
@@ -11,25 +11,11 @@ class DistrictTest {
     @Test
     public void save(){
         District district = new District();
-        district.setId("D01");
-        district.setId("Bac Tu Liem");
-////        District district1 = new District();
-////        district1.setId("AO2");
-////        district1.setName("nam tu liem");
-////        District district2 = new District();
-////        district2.setId("AO3");
-////        district2.setName("Ho tay");
-////        District district3 = new District();
-////        district3.setId("AO4");
-////        district3.setName("Truong Chinh");
-////        District district4 = new District();
-////        district4.setId("AO5");
-////        district4.setName("Lac Long Quan");
-//
+        district.setId("A01");
+        district.setName("Tran Thai Tong");
+
             districtRepository.save(district);
-////        districtRepository.save(district1);
-////        districtRepository.save(district2);
-////        districtRepository.save(district3);
-////        districtRepository.save(district4);
+        System.out.println(districtRepository.findAll());
+
     }
 }
